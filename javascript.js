2580730f166bc317f4036d43b4f3ba2c3c3dcf3e
adeshina9521 <abdullateef.adegoke@gmail.com>
2022-01-25 15:46:50 +0100

    new challenge

--- javascript.js
+++ javascript.js
@@ -2,4 +2,32 @@ var x = 2;
 var y = 7;
 var z = x + y;
 
-document.getElementById("demo").innerHTML= "the value of z is " + z; 
+// create an object
+// const is best use for object and array
+const person = {
+    firstName: "Adegoke",
+    lastName: "Abdullateef",
+    age: 32,
+    eyeColor: "black",
+    fullName: function (){
+        return this.firstName + " " + this.lastName ;
+    }
+};
+  const myName = person.fullName();
+
+document.getElementById("demo").innerHTML= "the value of z is " + myName; 
+
+
+// Event listner
+
+function displayDate() {
+    document.getElementById('demo1').innerHTML = Date();
+}
+
+// ARRAY
+
+const car = ["Toyota", 
+    "Benz", 
+    "Venza",
+    "BMW"
+] 
